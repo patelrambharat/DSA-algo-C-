47d1601c1eb6f9e781e7ada062b68d91d694c2da
patelrambharat <patelrambharat@gmail.com>
2022-05-30 16:56:55 +0530

    commit

---
+++ lecture19to22recursion/PhoneKeyboardProblemleetcode.cpp
@@ -0,0 +1,40 @@
+
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+
+class Solution {
+public:
+    void solve(string digits,string output,int index,vector<string>&ans,string mapping[]){
+        if(index >= digits.length()){
+            ans.push_back(output);
+            return;
+        }
+        int number = digits[index] -'0';
+        string value = mapping[number];
+        for(int i=0;i<value.length();i++){
+            output.push_back(value[i]);
+            solve(digits,output,index+1,ans,mapping);
+            output.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        string output = "";
+        int index =0; //base case
+        if(digits.length()==0){
+            return ans;
+        }
+        string mapping[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,output,index,  ans,mapping);
+        return ans;
+    }
+};
