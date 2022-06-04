267d2e75c15bbacdae3d2407d5d3d137f4de2989
patelrambharat <patelrambharat@gmail.com>
2022-06-04 22:12:32 +0530

    combination sum commit

---
+++ lecture23to 25 backtracking/3combinationSum.cpp
@@ -0,0 +1,69 @@
+
+// 39. Combination Sum
+// Medium
+
+// 11210
+
+// 241
+
+// Add to List
+
+// Share
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+// It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+// Example 1:
+
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.
+// Example 2:
+
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+// Example 3:
+
+// Input: candidates = [2], target = 1
+// Output: []
+
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target, vector<vector<int>> &output,
+               vector<int> &temp, int i) {
+        //base case
+        if(target == 0 ) {
+            //ans store
+            output.push_back(temp);
+            return ;
+        }
+        
+        if(target < 0 || i >= candidates.size()) 
+            return;
+        
+        //exclude
+        solve(candidates, target, output, temp, i+1);
+        
+        //include
+        temp.push_back(candidates[i]);
+        solve(candidates, target-candidates[i], output, temp, i);
+        temp.pop_back();
+      
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> output;
+        vector<int> temp;
+        
+        solve(candidates, target, output, temp, 0);
+        return output;
+        
+    }
+};
---
+++ lecture23to 25 backtracking/4.combinationsumII.cpp
@@ -0,0 +1,79 @@
+// 40. Combination Sum II
+// Medium
+
+// 5482
+
+// 145
+
+// Add to List
+
+// Share
+// Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+// Each number in candidates may only be used once in the combination.
+
+// Note: The solution set must not contain duplicate combinations.
+
+ 
+
+// Example 1:
+
+// Input: candidates = [10,1,2,7,6,1,5], target = 8
+// Output: 
+// [
+// [1,1,6],
+// [1,2,5],
+// [1,7],
+// [2,6]
+// ]
+// Example 2:
+
+// Input: candidates = [2,5,2,1,2], target = 5
+// Output: 
+// [
+// [1,2,2],
+// [5]
+// ]
+
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target, vector<vector<int>> &output,
+               vector<int> &temp, int index) {
+        //base case
+        if(target == 0 ) {
+            //ans store
+            output.push_back(temp);
+            return ;
+        }
+        
+        if(target < 0 || index >= candidates.size()) 
+            return;
+        
+        for(int i = index; i < candidates.size(); i++) {
+            
+            //special condition
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            
+            if(candidates[i] > target)
+                break;
+            
+            temp.push_back(candidates[i]);
+            solve(candidates, target-candidates[i], output, temp, i+1);
+            temp.pop_back();
+            
+        }
+        
+       
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        
+        vector<vector<int>> output;
+        vector<int> temp;
+        
+        solve(candidates, target, output, temp, 0);
+        return output;
+        
+    }
+};
