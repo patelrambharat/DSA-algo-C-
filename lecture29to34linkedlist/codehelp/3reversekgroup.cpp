c1b1ba6d0aae1f9b9c625b9d4f4051ddc809310b
Rambharat Patel <80194177+patelrambharat@users.noreply.github.com>
2022-06-19 06:23:11 +0530

    commit

---
+++ lecture29to34linkedlist/codehelp/3reversekgroup.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+// 25. Reverse Nodes in k-Group
+// Hard
+
+// 7561
+
+// 506
+
+// Add to List
+
+// Share
+// Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+// k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+// You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [2,1,4,3,5]
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5], k = 3
+// Output: [3,2,1,4,5]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is n.
+// 1 <= k <= n <= 5000
+// 0 <= Node.val <= 1000
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+       //base call
+    if(head == NULL) {
+        return NULL;
+    }
+    
+    //step1: reverse first k nodes
+    ListNode* next = NULL;
+    ListNode* curr = head;
+    ListNode* prev = NULL;
+    int count= 0;
+    
+    while( curr != NULL && count < k ) {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    
+    //step2: Recursion dekhlega aage ka 
+    if(next != NULL) {
+        head -> next = reverseKGroup(next,k);
+    }
+    
+    //step3: return head of reversed list
+    return prev;
+}
+};
